f6259134e26b6a958d05698e8c291614c9dd3d13
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2022-12-25 22:02:22 +0000

    btn function

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
 let result = document.querySelector(".result")
-
+let btn = document.querySelector(".btn")
+let input = document.querySelector(".input")
 //
-let pictures = ""
-//const ACCESS_KEY:  "DRyWQshwghkCo9Ls_D22kCtG0JYWnKgo3O-yElpbB38";
-const unsplashEndpoint = `https://api.unsplash.com/search/photos?client_id=${"DRyWQshwghkCo9Ls_D22kCtG0JYWnKgo3O-yElpbB38"}&query=`;
 
-const response = fetch(unsplashEndpoint + "monkeys")
+async function getPhoto(searchName){
+    let pictures = ""
+    //const ACCESS_KEY:  "DRyWQshwghkCo9Ls_D22kCtG0JYWnKgo3O-yElpbB38";
+    const unsplashEndpoint = `https://api.unsplash.com/search/photos?client_id=${"DRyWQshwghkCo9Ls_D22kCtG0JYWnKgo3O-yElpbB38"}&query=`;
 
-response
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-        //
-        let imagesArray = data.results;
+  
+    const response = fetch(unsplashEndpoint + searchName)
 
-        imagesArray.forEach(image => {
-            pictures += `<><img src="${image.urls.thumb}" /></>`
-        });
+    
 
-        result.innerHTML =pictures
-    })
+    response
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            //
+            let imagesArray = data.results;
+
+            imagesArray.forEach(image => {
+                pictures += `<img src="${image.urls.thumb}"/>`
+            });
+
+            result.innerHTML = pictures
+        })
+}
+
+
+// TO REVIEW 
+btn.addEventListener("click", () => {
+     getPhoto(prompt("enter item name"))
+})
+
+
+
