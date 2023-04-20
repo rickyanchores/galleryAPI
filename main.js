13b57c547eaa12a6d4d54ebc29c77ba980490090
RickyAnchores <ricky23anchores@gmail.com>
2023-04-20 19:43:34 +0100

    edit js

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 let result = document.querySelector(".result")
-let smallResult = document.querySelector(".smallResult")
-let bigResult = document.querySelector(".bigResult")
 let btn = document.querySelector(".btn")
 
 //
 
 async function getPhoto(searchName){
     let pictures = ""
-    let smallPictures = ""
-    let bigPictures = ""
     //const ACCESS_KEY:  "DRyWQshwghkCo9Ls_D22kCtG0JYWnKgo3O-yElpbB38";
     const unsplashEndpoint = `https://api.unsplash.com/search/photos?client_id=${"DRyWQshwghkCo9Ls_D22kCtG0JYWnKgo3O-yElpbB38"}&query=`;
   
@@ -23,37 +19,22 @@ async function getPhoto(searchName){
             let imagesArray = data.results ;
 
             imagesArray.forEach(image => {
-                pictures += `<img src="${image.urls.thumb}"/>
-                              <p>${image.description}</p>
-                              <p>Likes: ${image.likes}</p>
-                              <p>${image.user.first_name}</p>
-                              <p>${image.user.last_name}</p>
-                              <p>${image.user.location}</p>
-                              <button href="${image.links.download}">Download</button>`
+                pictures += `<div class="picture-container">
+                <img src="${image.urls.raw}"/>
+                <p>${image.description}</p>
+                <p>Likes: ${image.likes}</p>
+                <p>${image.user.first_name}</p>
+                <p>${image.user.last_name}</p>
+                <p>${image.user.location}</p>
+                <button href="${image.links.download}">Download</button>
+                
+                            </div>
+`
             });
 
-            //Fetch small image
-
-            let smallImagesArray = data.results
-
-                smallImagesArray.forEach(smallImage => {
-                    smallPictures += `<img src="${smallImage.urls.small}"/>
-                                        <p>${smallImage.description}</p>`
-            })
-       
-            //Fetch big image
-
-            let bigImagesArray = data.results;
-
-            bigImagesArray.forEach(bigImage => {
-                bigPictures += `<img src="${bigImage.urls.full} />
-                                        <p>${bigImage.description}</p>`
-            })
 
             // to RENDER
             result.innerHTML = pictures
-            smallResult.innerHTML = smallPictures;
-            bigResult.innerHTML = bigPictures
         })
 }
 
